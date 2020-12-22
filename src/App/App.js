22f6d56605dca83b39fb9cb6bbdeed055375e186
Yovador <yovador@gmail.com>
2020-12-22 14:49:53 +0100

    Craco install, setup hiérachie React

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './App.module.css';
+
+class App extends React.Component{
+
+
+  render(){
+    return (
+      <div className={styles.main}>
+        Blabla
+      </div>
+    );
+  }
+
+}
+
+export default App;
